dcbbc45619d43b1486dd0e0711cca7950636733a
NilanjanDaw <nilanjandaw@gmail.com>
2016-07-08 20:57:35 +0530

    Linux Change

--- land.php
+++ land.php
@@ -78,8 +78,8 @@
 
      ?>
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-      <?php require_once '/header_bar.html' ?>
-      <?php require_once '/sidebar.html' ?>
+      <?php require_once './header_bar.html' ?>
+      <?php require_once './sidebar.html' ?>
       <main class="mdl-layout__content mdl-color--grey-100">
         <div class="mdl-grid demo-content">
           <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
